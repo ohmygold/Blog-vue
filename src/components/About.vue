f1f71af8c93cb0e42aff04fa919d5ccb18afc0e0
ohmygold <499452621@qq.com>
2018-01-29 14:31:50 +0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="about">
+    <div class="about_left">
+        <h2>关于我</h2>
+        <p>ABOUT ME</p>
+        <ul>
+           <li>姓名:王伟</li>
+           <li>出生日期:1992/03/28</li>
+           <li>籍贯:四川南充</li>
+           <li>工作经验:一年</li>
+           <li>职能:WEB前端</li>
+           <li>毕业院校:西南交通大学希望学院</li>
+           <li>邮箱:499452621@qq.com</li>
+           <li>联系电话:15801308941</li>
+        </ul>
+    </div>
+    <div class="about_right">
+    
+    </div>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+         
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+.about
+ 
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="header">
+    <el-tabs type="border-card">
+        <el-tab-pane>
+        <span slot="label"><i class="el-icon-date"></i> 关于我</span>
+        我的行程
+        </el-tab-pane>
+        <el-tab-pane label="个人技能">1</el-tab-pane>
+        <el-tab-pane label="工作经验">2</el-tab-pane>
+        <el-tab-pane label="个人博客">3</el-tab-pane>
+    </el-tabs>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
